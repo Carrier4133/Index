5e4ca34a8833981dbf4f6250b6c69b7055458b20
Carrier4133 <50059895+Carrier4133@users.noreply.github.com>
2026-02-24 21:35:15 -0800

    In-Progress Localization file + Looping the Rooms

---
+++ localization.lua
@@ -0,0 +1,10 @@
+return {
+	["descriptions"] = {
+		["Joker"] = {
+			["j_index_V1"] = {
+				["name"] = "V1",
+				["text"] = "Scored #1# take away $#2# for {X:mult,C:white}X#3#{} Mult",
+			},
+		},
+	},
+}
